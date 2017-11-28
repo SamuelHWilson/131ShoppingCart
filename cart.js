5cb83356a759aba4a3d927bc20696cb2d0ce8b3e
Unknown <swilsondev@outlook.com>
2017-11-28 11:46:56 -0600

    Furthered layout and began Vue code.
     - Finished prototype layout for menu-items, and menu.
     - Finished Vue for storing and displaying items.

---
+++ cart.js
@@ -0,0 +1,10 @@
+var Main = new Vue({
+    el: "#main",
+    data: {
+        items: [
+            {name: "Fettuccine Alfredo", price: "11.50", stock: 6, desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ut labore et dolore aliqua.", imgSrc: "img/alfredo.png", ordered: 0},
+            {name: "Pizza", price: "13.99", stock: 8, desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed  ut labore et dolore aliqua.", imgSrc: "img/pizza.png", ordered: 0},
+            {name: "Ham Carbonara", price: "11.50", stock: 3, desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ut labore et dolore aliqua.", imgSrc: "img/carbonara.png", ordered: 0}
+        ]
+    }
+});
